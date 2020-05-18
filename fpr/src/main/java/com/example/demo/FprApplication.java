65f83f0ae3a7d48e1ea782666aa93be4432e525e
Lenovo <Lenovo@Lenovo-pc>
2020-05-18 19:14:11 +0530

    first commit.

---
+++ fpr/src/main/java/com/example/demo/FprApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FprApplication.class, args);
+	}
+
+}
